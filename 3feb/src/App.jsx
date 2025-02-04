80b0d79f2ce8c98c69320c4747080aedae53f44e
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-04 15:42:37 +0530

    multi input handle

--- 3feb/src/App.jsx
+++ 3feb/src/App.jsx
@@ -1,16 +1,17 @@
 import { useEffect,useState } from "react"
 const App=()=>{
     const [cnt,setcnt]=useState(0);
+    const [mult,setmult]=useState(0);
 useEffect(()=>{
-    setTimeout(()=>{
-        setcnt(cnt+1);
-    },2000)
- 
-})
+ setmult(cnt*2)
+},[cnt])
 
     return(
         <>
         <h1>My count {cnt}</h1>
+        <h1>My multi {mult}</h1>
+
+    <button onClick={()=>setcnt(cnt+1)}>Click Here</button>
         </>
     )
 }
