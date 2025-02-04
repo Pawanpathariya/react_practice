131eac18e97497d2f9ec1b3d6a9ed22630d67467
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-04 15:31:48 +0530

    multi input handle

--- 3feb/src/App.jsx
+++ 3feb/src/App.jsx
@@ -1,32 +1,17 @@
-
-import { useState } from "react";
-
+import { useEffect,useState } from "react"
 const App=()=>{
-  const [data,setData]=useState({})
-  const handleInput=(e)=>{
-    const name=e.target.name;
-    const value=e.target.value;
+    const [cnt,setcnt]=useState(0);
+useEffect(()=>{
+    setTimeout(()=>{
+        setcnt(cnt+1);
+    },2000)
+ 
+})
 
-    setData({...data,[name]:value})
-    
-  }
-
-  const handleSubmit=(e)=>{
-    console.log(data);
-    
-  }
-  return(
-    <>
-   <h1>Application form</h1>
-Enter Name : <input type="text" name="name" value={data.name} onChange={handleInput} /> <br />
-Enter city : <input type="text" name="city" value={data.city} onChange={handleInput} /> <br /> 
-Enter Subject : <input type="text" name="subject" value={data.subject} onChange={handleInput} /> <br /> 
-Enter Fees : <input type="text" name="fees"value={data.fees}  onChange={handleInput} /> <br />
-
-
-<button onClick={handleSubmit}>Submit</button>
-    </>
-  )
-} 
-
-export default App;
+    return(
+        <>
+        <h1>My count {cnt}</h1>
+        </>
+    )
+}
+export default App
